39c58689bb27f37dd6806095f42ba4aa638ae405
Nathan Leiby <nathanleiby@gmail.com>
2020-03-22 23:37:37 -0700

    menu: prevent mouse from stealing focus

---
+++ Assets/IgnoreMouseFocus.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+ 
+public class IgnoreMouseFocus : MonoBehaviour
+         // https://answers.unity.com/questions/943854/ui-46-disable-mouse-from-stealing-focus.html
+{
+     GameObject lastselect;
+     void Start()
+     {
+         lastselect = new GameObject();
+     }
+     // Update is called once per frame
+     void Update () {         
+         if (EventSystem.current.currentSelectedGameObject == null)
+         {
+             EventSystem.current.SetSelectedGameObject(lastselect);
+         }
+         else
+         {
+             lastselect = EventSystem.current.currentSelectedGameObject;
+         }
+     }
+}
