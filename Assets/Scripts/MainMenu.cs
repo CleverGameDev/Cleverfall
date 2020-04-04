3392345c6c17defbd80292d8df5cb2f6f7a2c064
Nathan Leiby <nathanleiby@gmail.com>
2020-04-03 18:53:40 -0700

    allow selecting a level

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    private string[] levels = new string[]{
-        // These must match the exact scene names
-        "SandyLevel",
-        "DungeonLevel",
-    };
-
-    public void PlayGame() {
-        string randomLevel = levels[Random.Range(0, levels.Length)]; 
-        SceneManager.LoadScene(randomLevel);
-    }
-
     public void QuitGame() {
         Debug.Log("Quit");
         Application.Quit();
