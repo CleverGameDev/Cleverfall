3860a8cab4ca1fc3b3232dc917bde15b306e456c
Nathan Leiby <nathanleiby@gmail.com>
2020-04-05 00:02:41 -0700

    WIP: allow players to select a character

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharacterSelect : MonoBehaviour
+{
+    [SerializeField, Tooltip("UI to choose player")]
+    public PlayerChooser[] choosers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check how many players (player inputs?) are present
+        GameObject[] players = GameObject.FindGameObjectsWithTag("HumanPlayer");
+        for (int i=0; i < 4; i++) {
+            TextMeshProUGUI c = choosers[i].GetComponent<TextMeshProUGUI>();
+            if (i < players.Length) {
+                string character = players[i].GetComponent<HumanPlayer>().GetCharacter();
+                c.text = character + "\nP" + (i+1);
+            } else {
+                c.text = "";
+            }
+        }
+
+        // print 
+
+        // create a select UX for each player
+        
+    }
+}
---
+++ Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HumanPlayer : MonoBehaviour
+{
+    [SerializeField, Tooltip("Player Input")]
+    public PlayerInput playerInput;
+
+    private string[] characters = {
+        "larry", 
+        "curly", 
+        "moe"
+    };
+    private int selectedCharacterIdx = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player joined: " + playerInput.playerIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // TODO: Only allow making these changes on the CharacterSelect screen
+    public void OnNextCharacter() {
+        selectedCharacterIdx = (selectedCharacterIdx + 1 ) % characters.Length;
+    }
+    public void OnPreviousCharacter() {
+        selectedCharacterIdx = (characters.Length + selectedCharacterIdx - 1 ) % characters.Length;
+    }
+
+    public string GetCharacter() {
+        return characters[selectedCharacterIdx];
+    }
+}
