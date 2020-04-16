f28dd7f247c95ada78c9757b455aaccafcd38c95
Nathan Leiby <nathanleiby@gmail.com>
2020-04-16 00:54:40 -0700

    add 45s round timer

--- Assets/CombatManager.cs
+++ Assets/CombatManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CombatManager : MonoBehaviour
-{
+{    
+    float roundLength = 45; // seconds
+    float currCountdownValue; 
+
+    CombatGUI combatGUI;
+    EndCombatUI endCombatUI;
+
     void Awake()
     {
         Debug.Log("CombatManager:Awake()");
@@ -14,14 +20,54 @@ public class CombatManager : MonoBehaviour
             HumanPlayer hp = players[i].GetComponent<HumanPlayer>();
             hp.SetupCombat();
         }
+
+        // Get references to other prefab-instances, which can't be set via editor
+        combatGUI = GameObject.Find("CombatGUI").GetComponent<CombatGUI>();
+        Assert.IsNotNull(combatGUI);
+        endCombatUI = GameObject.Find("EndCombatUI").GetComponent<EndCombatUI>();
+        Assert.IsNotNull(endCombatUI);
+
+        startCombat();
     }
 
-    // Update is called once per frame
-    void Update()
+    // startCombat sets up victory conditions for combat
+    void startCombat() {
+        combatGUI.Show();
+        endCombatUI.Hide();
+
+        // One end condition is a timer, and the player with the most kills wins
+        StartCoroutine(startCountdown(roundLength));
+
+        // TODO: Add support for another game type, where "players start with n lives"
+        // if exactly <=1 player has >0 lives then round over!
+        // ... if only 1 survivor: they win
+        // ... if 0 survivors: draw
+    }
+    
+    public IEnumerator startCountdown(float countdownValue)
     {
-        
+        currCountdownValue = countdownValue;
+        while (true) {
+            Debug.Log("Countdown: " + currCountdownValue);
+            combatGUI.SetTimeRemaining(currCountdownValue);
+            if (currCountdownValue <= 0) {
+                break;
+            }
+            
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+
+        endCombat();
     }
 
+    void endCombat() {
+        Debug.Log("CombatManager:EndCombat()");
+
+        combatGUI.Hide();
+        endCombatUI.Show();
+    }
+   
     void OnDestroy() {
         Debug.Log("CombatManager:OnDestroy()");
         // Cleanup player avatars
@@ -32,7 +78,4 @@ public class CombatManager : MonoBehaviour
             hp.CleanupCombat();
         }
     }
-
-
-
 }
---
+++ Assets/EndCombatUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCombatUI : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    public void Show() {
+        panel.SetActive(true);
+    }
+
+    public void Hide() {
+        panel.SetActive(false);
+    }
+}
