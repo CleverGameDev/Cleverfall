7229a8a13b046e4f9f4bd2f4b36fd8562513e44d
Benjamin Li <benlibenli@gmail.com>
2020-04-23 15:08:20 -0700

    NoxiousGas can make players DIE

--- Assets/Scripts/NoxiousGas.cs
+++ Assets/Scripts/NoxiousGas.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoxiousGas : MonoBehaviour
-{
+public class NoxiousGas : MonoBehaviour {
+
+    private Hashtable playerTimeInGas;
+    private int maxPlayerExposureTime = 400;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        playerTimeInGas = new Hashtable();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            PlayerAvatar player = other.gameObject.GetComponent<PlayerAvatar>();
+            playerTimeInGas[player] = 0;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            PlayerAvatar player = other.gameObject.GetComponent<PlayerAvatar>();
+
+            // if (!playerTimeInGas.Contains(player)) {
+            //     playerTimeInGas[player] = 0;
+            // }
+
+            playerTimeInGas[player] = (int)playerTimeInGas[player] + 1;
+            if ((int)playerTimeInGas[player] > maxPlayerExposureTime) {
+                player.Die();
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            PlayerAvatar player = other.gameObject.GetComponent<PlayerAvatar>();
+            playerTimeInGas[player] = 0;
+        }
     }
 }
