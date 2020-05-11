8baa3422b26544bd6898a5e8f558b51bcbf0eb12
Benjamin Li <benlibenli@gmail.com>
2020-05-11 12:00:02 -0700

    Noxious Gas has some transparency, and renders above spawn points

--- Assets/Scripts/NoxiousGas.cs
+++ Assets/Scripts/NoxiousGas.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class NoxiousGas : MonoBehaviour {
 
-    public ParticleSystem particleSystem;
-
-    // Start is called before the first frame update
-    void Start() {
-        this.particleSystem = GetComponent<ParticleSystem>();
-    }
-
     void OnParticleCollision(GameObject other) {
+
         if (other.gameObject.tag == "Player") {
             PlayerAvatar player = other.gameObject.GetComponent<PlayerAvatar>();
 
