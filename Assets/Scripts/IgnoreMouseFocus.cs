30eaf284aabd750a722387beffc09b965ca665a0
Nathan Leiby <nathanleiby@gmail.com>
2020-05-28 00:19:44 -0700

    ignore mouse input, remove mouse cursor

--- Assets/Scripts/IgnoreMouseFocus.cs
+++ Assets/Scripts/IgnoreMouseFocus.cs
@@ -5,9 +5,14 @@ using UnityEngine.EventSystems;
 public class IgnoreMouseFocus : MonoBehaviour {
     GameObject lastselect;
     void Start() {
-        lastselect = new GameObject();
+        Debug.Log("Ignore mouse interations");
+        // https://answers.unity.com/questions/1507986/disable-mouse-input-and-cursor-in-game.html
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
+        lastselect = new GameObject();
     }
+
     // Update is called once per frame
     void Update() {
         if (EventSystem.current.currentSelectedGameObject == null) {
