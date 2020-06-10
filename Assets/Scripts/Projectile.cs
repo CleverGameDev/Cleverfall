4a99245f7248e45a4bd7c2310de17242e3a06422
Hout Nga <houtnga@Houts-MBP.attlocal.net>
2020-06-10 00:28:01 -0700

    Give projectiles a life and owner, support more damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-	public float xVel;
-	Rigidbody2D rb;
+	public float speed;
+	public int damage;
+	public int duration;
+
+	private Rigidbody2D rb;
+	private PlayerAvatar owner;
+	private int remainingDuration;
+
 
 	void Start() {
-		rb = GetComponent<Rigidbody2D>();
+		this.rb = this.GetComponent<Rigidbody2D>();
+		this.remainingDuration = duration;
 	}
 
-	public void Initialize(Vector2 direction) {
-		rb.velocity = direction * xVel;
+	void Update() {
+		if (this.remainingDuration <= 0) {
+			Destroy(gameObject);
+		}
+		this.remainingDuration--;
+	}
+
+	public void SetDirection(Vector2 direction) {
+		if (!rb) {
+			this.rb = this.GetComponent<Rigidbody2D>();
+		}
+		this.rb.velocity = direction * this.speed;
+	}
+
+	public void SetOwner(PlayerAvatar pa) {
+		this.owner = pa;
+	}
+
+	public PlayerAvatar GetOwner() {
+		return this.owner;
 	}
 
 	private void OnCollisionEnter2D(Collision2D other) {
         if (other.collider.tag == "Player") {
             PlayerAvatar player = other.gameObject.GetComponent<PlayerAvatar>();
 
-            player.TakeDamage(1, this.gameObject);
+            player.TakeDamage(damage, this.gameObject);
         }
         Destroy(gameObject);
     }
